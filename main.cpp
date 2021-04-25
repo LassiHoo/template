7c470d98ba5214787987ab6857127255a3a9facf
Lassi Hirvonen <lassi@hirvonen.net>
2021-04-25 10:24:26 +0300

    added debug level handling

--- main.cpp
+++ main.cpp
@@ -6,20 +6,47 @@
 #include <plog/Formatters/TxtFormatter.h>
 #include <plog/Appenders/ColorConsoleAppender.h>
 
+void setDebugLevel(std::string &level)
+{
+    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+    if (level == "DEBUG")
+    {
+        plog::init(plog::debug, &consoleAppender);
+    }
+    else if (level == "ERROR")
+    {
+        plog::init(plog::error, &consoleAppender);
+    }
+    else if (level == "VERBOSE")
+    {
+        plog::init(plog::verbose, &consoleAppender);
+    }
+    else
+    {
+        plog::init(plog::error, &consoleAppender);
+        PLOG_ERROR<<"debug level is not set, level is forced to error";        
+    }
+}
+
 int main (int arc, char **argv)
 {
-   
-    static plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
-    plog::init(plog::verbose, &consoleAppender);
-
-
+    std::string debug_level = "NONE";
     cxxopts::Options options("Home automation","home automation application");
     PLOG_DEBUG<<"version minor: "<<homeAutomation_VERSION_MINOR;
     PLOG_DEBUG<<"version major: "<<homeAutomation_VERSION_MAJOR;
     options.add_options()
-    ("l, level","debug level")
-    ("d, debug","enable debugging");
+    ("d, level","debug level: DEBUG, ERROR, VERBOSE" , cxxopts::value<std::string>())
+    ("h, help","");
     auto parsed_options = options.parse(arc,argv);
-
+   
+    if (parsed_options.count("level"))
+      debug_level = parsed_options["level"].as<std::string>();
+    setDebugLevel(debug_level);
+     if (parsed_options.count("help"))
+    {
+      PLOG_DEBUG << options.help();
+      exit(0);
+    }
+    PLOG_DEBUG<<"starting the application";
     return 0;
 }
